045a48a894e55bff5c5dec6f0f8b437441d96252
Manuel Šarić <manujelko@gmail.com>
2022-10-04 20:13:47 +0200

    Fetching lists of posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,18 @@
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({ posts: [{ title: 'First Post', content: 'This is the first post!' }] });
+    res.status(200).json({
+        posts: [
+            {
+                _id: 1,
+                title: 'First Post',
+                content: 'This is the first post!',
+                imageUrl: 'images/duck.jpg',
+                creator: {
+                    name: 'Maximilian'
+                },
+                date: new Date()
+            }
+        ]
+    });
 };
 
 exports.createPost = (req, res, next) => {
