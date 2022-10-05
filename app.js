79e5934675ecada6a481deaee47f86340bc70c7e
Manuel Šarić <manujelko@gmail.com>
2022-10-05 10:47:40 +0200

    Uploading images

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const path = require("path");
 
-const bodyParser = require("body-parser");
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
-app.use(bodyParser.json());
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
@@ -32,7 +58,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect("mongodb://localhost:27017/messages?retryWrites=true")
-  .then(app.listen(8080))
-  .catch((err) => {
-    console.log(err);
-  });
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
--- models/post.js
+++ models/post.js
@@ -17,7 +17,7 @@ const postSchema = new Schema(
     },
     creator: {
       type: Object,
-      requried: String,
+      required: String,
     },
   },
   { timestamps: true }
