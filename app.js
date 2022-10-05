e8f47ddd5bee312e35f3fe998b2389cac42d8e1d
Manuel Šarić <manujelko@gmail.com>
2022-10-05 09:45:32 +0200

    Setting up a post model

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -16,4 +17,4 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect('mongodb://localhost:27017/messages?retryWrites=true').then(app.listen(8080)).catch(err => {console.log(err)});
