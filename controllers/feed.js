ab1ae327b101b363f33d613368335c68d6a214ba
Manuel Šarić <manujelko@gmail.com>
2022-10-05 09:37:15 +0200

    Adding server side validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,26 +1,40 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                _id: 1,
-                title: 'First Post',
-                content: 'This is the first post!',
-                imageUrl: 'images/duck.jpg',
-                creator: {
-                    name: 'Maximilian'
-                },
-                date: new Date()
-            }
-        ]
-    });
+  res.status(200).json({
+    posts: [
+      {
+        _id: 1,
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/duck.jpg",
+        creator: {
+          name: "Maximilian",
+        },
+        date: new Date(),
+      },
+    ],
+  });
 };
 
 exports.createPost = (req, res, next) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    // Create post in db
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: { _id: new Date().toISOString(), title: title, content: content, creator: {}, createdAt: new Date() }
-    });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ message: "Validation failed, entered data is incorrect" });
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {},
+      createdAt: new Date(),
+    },
+  });
 };
