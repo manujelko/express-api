c9b1768fc3fbfab015e22f3f79a1304f54364942
Manuel Šarić <manujelko@gmail.com>
2022-10-05 09:58:06 +0200

    Static images & error handling

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const mongoose = require('mongoose');
+const path = require("path");
 
-const feedRoutes = require('./routes/feed');
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const feedRoutes = require("./routes/feed");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
-mongoose.connect('mongodb://localhost:27017/messages?retryWrites=true').then(app.listen(8080)).catch(err => {console.log(err)});
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect("mongodb://localhost:27017/messages?retryWrites=true")
+  .then(app.listen(8080))
+  .catch((err) => {
+    console.log(err);
+  });
